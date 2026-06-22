a8ec7aff50729906067dc1204545f5f726ae3116
Aziz Toulgui <aziztoulgui20@gmail.com>
2026-06-22 14:29:41 +0100

    Add a completed UI for the shopping list component

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tee" />
+      <ShoppingListItem name="Tee" isCompleted />
       <ShoppingListItem name="Sugar" />
     </View>
   );
--- components/shoppingListItem.tsx
+++ components/shoppingListItem.tsx
@@ -3,9 +3,10 @@ import { theme } from "../theme";
 
 type Props = {
   name: string;
+  isCompleted?: boolean;
 };
 
-export function ShoppingListItem({ name }: Props) {
+export function ShoppingListItem({ name, isCompleted }: Props) {
   const handleDelete = () => {
     Alert.alert(
       `Are you sure you want to delete ${name}?`,
@@ -24,12 +25,28 @@ export function ShoppingListItem({ name }: Props) {
     );
   };
   return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemText}>{name}</Text>
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedItemText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
       <TouchableOpacity
         onPress={() => handleDelete()}
         activeOpacity={0.8}
-        style={styles.button}
+        style={[
+          styles.button,
+          isCompleted ? styles.completedButton : undefined,
+        ]}
+        disabled={isCompleted}
       >
         <Text style={styles.buttonText}>Delete</Text>
       </TouchableOpacity>
@@ -40,18 +57,30 @@ export function ShoppingListItem({ name }: Props) {
 const styles = StyleSheet.create({
   itemContainer: {
     borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
+    borderBottomColor: theme.colorCerulean,
     paddingHorizontal: 8,
     paddingVertical: 16,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
   },
+  completedContainer: {
+    backgroundColor: theme.colorLightGray,
+    borderBottomColor: theme.colorLightGray,
+  },
   itemText: { fontSize: 18, fontWeight: 200 },
+  completedItemText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGray,
+    color: theme.colorGray,
+  },
   button: {
     backgroundColor: theme.colorBlack,
     padding: 8,
     borderRadius: 6,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGray,
   },
   buttonText: {
     color: theme.colorWhite,
